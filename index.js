0164428c18887379cbe3a828a8567625c882b3b6
obelisco-gif <obelisco@tomasguillen.net>
2026-07-08 13:04:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const { chromium } = require("playwright");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Playwright OK");
+    res.send("Playwright API");
+});
+
+app.get("/test", async (req, res) => {
+    const browser = await chromium.launch({
+        headless: true
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto("https://web.whatsapp.com");
+
+    const title = await page.title();
+
+    await browser.close();
+
+    res.json({
+        ok: true,
+        title
+    });
 });
 
 app.listen(3000, () => {
-    console.log("Servidor iniciado en puerto 3000");
+    console.log("Servidor iniciado");
 });
